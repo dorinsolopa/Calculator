8234e8e1803ead01c166fa89452d8dc581c20388
dorinsolopa <d.solopa@gmail.com>
2020-07-27 15:47:33 +0300

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./components/LandingPage";
 function App() {
   return (
     <div className="container">
-    
-   <LandingPage/>
+      <LandingPage />
     </div>
   );
 }
